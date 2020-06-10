e1cb9ddbc50ad1a2de75c0782b5e3aee23ff12e3
Reyes <uidk2557@contiwan.com>
2020-06-10 12:45:30 -0500

    Se ajusto el codigo para imprimir en modo lista

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,20 @@
+"""
+#see what is inside of folder
+import os
+
+entries = os.listdir('python-random-quote/')
+
+print(entries)
+"""
 def main():
   print("Keep it logically awesome.")
 
-  f = open("quotes.txt","r")
+  f = open("python-random-quote/quotes.txt","r") #changed because of python doesn't find file
   quotes = f.readlines()
   f.close()
 
-  print(quotes)
+  for item in quotes:
+    print(item)  #making list of lines from file
 
 
 if __name__== "__main__":
